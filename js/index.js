7753a7e750b1db9ae07b24a6817fb887c3cd6abe
haikalsh22 <haikalshahrim2305@gmail.com>
2025-01-20 23:14:14 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'NAK BAGITAU NII'.split('')
+const text = 'NAK BAGITAU NIII'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
